1c78536005e7e7fb2cf3002f600877cc6c7c2097
SUWA Shigeki <shigeki.suwa@ztrehagem.dev>
2025-04-29 02:19:30 +0900

    chore: lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,44 @@
 import js from "@eslint/js";
-import globals from "globals";
+import gitignore from "eslint-config-flat-gitignore";
+import jsxA11y from "eslint-plugin-jsx-a11y";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import security from "eslint-plugin-security";
+import sonarjs from "eslint-plugin-sonarjs";
 import tseslint from "typescript-eslint";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default tseslint.config(
-  { ignores: ["dist", "worker-configuration.d.ts"] },
+  gitignore(),
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: ["dist", "worker-configuration.d.ts"],
+  },
+  {
     files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
+      sonarjs.configs.recommended,
+      security.configs.recommended,
+      jsxA11y.flatConfigs.recommended,
+    ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+      },
     },
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      "simple-import-sort": simpleImportSort,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
+      "no-console": "warn",
+      "no-debugger": "error",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "simple-import-sort/imports": "warn",
     },
   },
 );
