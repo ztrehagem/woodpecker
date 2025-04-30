c7da0b4edf8d9f92f2fdb5717f67e0da58e6eaeb
SUWA Shigeki <shigeki.suwa@ztrehagem.dev>
2025-04-30 23:55:00 +0900

    feat: add middlewares

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,5 +1,10 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { secureHeaders } from "hono/secure-headers";
 const app = new Hono<{ Bindings: Env }>();
+
+app.use(logger());
+app.use(secureHeaders());
 
 app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
 
