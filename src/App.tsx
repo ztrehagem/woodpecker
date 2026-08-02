cedd9a71704739823f01ae9deadfae76ccf7a40c
SUWA Shigeki <shigeki.suwa@ztrehagem.dev>
2026-08-02 23:40:39 +0900

    able to signin/out

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,96 @@
 import type React from "react";
+import { BrowserOAuthClient } from "@atproto/oauth-client-browser";
+import atpClientMetadata from "../public/atp-client-metadata.json";
+
+const oauthClient = await BrowserOAuthClient.load({
+  clientId: import.meta.env.DEV
+    ? `http://localhost?redirect_uri=${encodeURIComponent("http://127.0.0.1:5174/callback")}`
+    : "https://woodpecker.ztrehagem.com/atp-client-metadata.json",
+  handleResolver: "https://bsky.social",
+});
+
+const oauthResult = await oauthClient.init();
 
 export default function App(): React.ReactElement {
   return (
-    <h1>Woodpecker</h1>
-  )
+    <>
+      <h1>Woodpecker</h1>
+
+      <hr />
+
+      <div>
+        <h2>Client Metadata</h2>
+        <pre>{JSON.stringify(atpClientMetadata, null, 2)}</pre>
+      </div>
+
+      <hr />
+
+      {oauthResult ? (
+        <div>
+          {oauthResult.state ? (
+            <div>
+              {oauthResult.session.sub} was successfully authenticated (state:{" "}
+              {oauthResult.state})
+            </div>
+          ) : (
+            <div>
+              {oauthResult.session.sub} was restored (last active session)
+            </div>
+          )}
+          <SignOutForm sub={oauthResult.session.sub} />
+        </div>
+      ) : (
+        <SignInForm />
+      )}
+    </>
+  );
+}
+
+function SignInForm(): React.ReactElement {
+  const action = async (fd: FormData) => {
+    const handle = (fd.get("handle") as string).trim();
+
+    try {
+      await oauthClient.signIn(handle, {
+        state: "DUMMY_STATE",
+      });
+      // Never executed
+    } catch (error) {
+      console.log(
+        'The user aborted the authorization process by navigating "back"',
+        error,
+      );
+    }
+  };
+
+  return (
+    <form
+      action={action}
+      noValidate
+      name="signin"
+      style={{
+        display: "inline-grid",
+        gap: "8px 12px",
+        grid: "auto-flow auto / repeat(2, auto)",
+      }}
+    >
+      <label htmlFor="handle">Handle *</label>
+      <input
+        type="text"
+        id="handle"
+        name="handle"
+        placeholder="user.bsky.social"
+        required
+      />
+      <button type="submit">Sign In</button>
+    </form>
+  );
+}
+
+function SignOutForm({ sub }: { sub: string }): React.ReactElement {
+  return (
+    <button type="button" onClick={() => oauthClient.revoke(sub)}>
+      Sign Out
+    </button>
+  );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), cloudflare()],
-})
+
+  server: {
+    host: "127.0.0.1", // for atproto OAuth callback to work on local dev
+  },
+});
