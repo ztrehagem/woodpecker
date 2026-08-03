33cb0903b44fd11369bcf8fee4e777b41b0a5187
SUWA Shigeki <shigeki.suwa@ztrehagem.dev>
2026-08-03 23:55:59 +0900

    make atp-client-metadata dynamic

--- worker/index.ts
+++ worker/index.ts
@@ -1,12 +1,13 @@
+import { createAtpClientMetadata } from "./create-atp-client-metadata.ts";
+
 export default {
   fetch(request) {
     const url = new URL(request.url);
 
-    if (url.pathname.startsWith("/api/")) {
-      return Response.json({
-        name: "Cloudflare",
-      });
+    if (url.pathname == "/atp-client-metadata.json") {
+      return Response.json(createAtpClientMetadata(request));
     }
+
     return new Response(null, { status: 404 });
   },
 } satisfies ExportedHandler<Env>;
