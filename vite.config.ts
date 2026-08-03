9a412944889b19b14378df2b4a0b2ae44828b1a7
SUWA Shigeki <shigeki.suwa@ztrehagem.dev>
2026-08-04 08:01:01 +0900

    add tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react(), cloudflare(), tailwindcss()],
 
   server: {
     host: "127.0.0.1", // for atproto OAuth callback to work on local dev
