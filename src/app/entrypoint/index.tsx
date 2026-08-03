30d36b8aa3095765607984923f5e3a44de3747de
SUWA Shigeki <shigeki.suwa@ztrehagem.dev>
2026-08-03 19:49:26 +0900

    show self profile

--- src/app/entrypoint/index.tsx
+++ src/app/entrypoint/index.tsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router/dom";
 import "../styles/globals.css";
 import { router } from "../routes";
+import ErrorBoundary from "#src/shared/ui/error-boundary.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ErrorBoundary fallback={"Something went wrong!"}>
+      <RouterProvider router={router} />
+    </ErrorBoundary>
   </StrictMode>,
 );
--- src/features/auth/lib/oauth-client.ts
+++ src/features/auth/lib/oauth-client.ts
@@ -1,12 +1,16 @@
 import { BrowserOAuthClient, type ClientMetadata } from "@atproto/oauth-client-browser";
 import atpClientMetadata from "#/public/atp-client-metadata.json";
 import { use } from "react";
+import { CachedClient } from "./cached-client";
 
 const oauthClientPromise = import.meta.env.DEV
-  ? BrowserOAuthClient.load({
-      clientId: `http://localhost?redirect_uri=${encodeURIComponent(`${location.origin}/callback`)}`,
-      handleResolver: "https://bsky.social",
-    })
+  ? (() => {
+      const scopes = ["atproto", "rpc:app.bsky.actor.getProfile?aud=*"];
+      return BrowserOAuthClient.load({
+        clientId: `http://localhost?redirect_uri=${encodeURIComponent(`${location.origin}/callback`)}&scope=${encodeURIComponent(scopes.join(" "))}`,
+        handleResolver: "https://bsky.social",
+      });
+    })()
   : Promise.resolve(
       new BrowserOAuthClient({
         clientMetadata: atpClientMetadata as ClientMetadata,
@@ -24,4 +28,21 @@ const oauthResultPromise = oauthClientPromise.then((client) => client.init());
 
 export function useOAuthResult(): OAuthResult | null {
   return use(oauthResultPromise);
+}
+
+const cachedClientPromise = oauthResultPromise.then((result) => {
+  if (result == null) {
+    return null;
+  }
+  return new CachedClient(result.session);
+});
+
+export function useCachedClient(): CachedClient {
+  const client = use(cachedClientPromise);
+
+  if (client == null) {
+    throw new Error("No cached client available. User is not authenticated.");
+  }
+
+  return client;
 }
