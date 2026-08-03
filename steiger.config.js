78fa2d5746fb1739a31bccccc39fa5f7b5beed67
SUWA Shigeki <shigeki.suwa@ztrehagem.dev>
2026-08-03 18:28:50 +0900

    install lexicons

--- steiger.config.js
+++ steiger.config.js
@@ -2,4 +2,13 @@
 import { defineConfig } from "steiger";
 import fsd from "@feature-sliced/steiger-plugin";
 
-export default defineConfig([...fsd.configs.recommended]);
+export default defineConfig([
+  ...fsd.configs.recommended,
+  {
+    // disable the `public-api` rule for files in the Shared layer
+    files: ["./src/shared/**"],
+    rules: {
+      "fsd/public-api": "off",
+    },
+  },
+]);
