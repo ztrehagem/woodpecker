0f8d83fe98fd61e6fbd8ca3883b043b445c5564c
SUWA Shigeki <shigeki.suwa@ztrehagem.dev>
2026-08-07 01:11:02 +0900

    tweak steiger

--- steiger.config.js
+++ steiger.config.js
@@ -2,4 +2,13 @@ import fsd from "@feature-sliced/steiger-plugin";
 // ./steiger.config.js
 import { defineConfig } from "steiger";
 
-export default defineConfig([...fsd.configs.recommended]);
+export default defineConfig([
+  ...fsd.configs.recommended,
+  {
+    // disable the `public-api` rule for files in the Shared layer
+    files: ["./src/**"],
+    rules: {
+      "fsd/insignificant-slice": "off",
+    },
+  },
+]);
