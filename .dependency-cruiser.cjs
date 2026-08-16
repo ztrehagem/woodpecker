0b011b8d1ce23c88aca60312cb8be85292a2b441
SUWA Shigeki <shigeki.suwa@ztrehagem.dev>
2026-08-16 19:59:01 +0900

    fix depcruise rules

--- .dependency-cruiser.cjs
+++ .dependency-cruiser.cjs
@@ -142,7 +142,7 @@ module.exports = {
         "from.pathNot re of the not-to-dev-dep rule in the dependency-cruiser configuration",
       from: {
         path: "^(src)",
-        pathNot: "[.](?:spec|test)[.](?:js|mjs|cjs|jsx|ts|mts|cts|tsx)$",
+        pathNot: "^(?:src/test/|.*[.](?:spec|test)[.](?:js|mjs|cjs|jsx|ts|mts|cts|tsx)$)",
       },
       to: {
         dependencyTypes: ["npm-dev"],
