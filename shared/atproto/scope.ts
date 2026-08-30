904d5230a008b07e087b1187da77cd61c48c4708
SUWA Shigeki <shigeki.suwa@ztrehagem.dev>
2026-08-30 11:46:54 +0900

    add lexicon

--- shared/atproto/scope.ts
+++ shared/atproto/scope.ts
@@ -1,6 +1,8 @@
 export const scopes = [
   "atproto",
   "rpc:app.bsky.actor.getProfile?aud=*",
+  "rpc:app.bsky.actor.getPreferences?aud=*",
+  "rpc:app.bsky.actor.putPreferences?aud=*",
   "rpc:app.bsky.feed.getTimeline?aud=*",
   "rpc:app.bsky.feed.getAuthorFeed?aud=*",
   "rpc:app.bsky.feed.getPostThread?aud=*",
